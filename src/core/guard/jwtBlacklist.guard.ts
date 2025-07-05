04bca92155fc941ae23df4d3041d357d359ff4af
wonjjee <yoteamo7@naver.com>
2025-07-05 14:18:48 +0900

    feat: added JwtBlacklistGuard

---
+++ src/core/guard/jwtBlacklist.guard.ts
@@ -0,0 +1,44 @@
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Request } from 'express';
+import { JwtService } from '../jwt/jwt.service';
+import { IS_PUBLIC_KEY } from '../decorator/public.decorator';
+import { Reflector } from '@nestjs/core';
+
+@Injectable()
+export class JwtBlacklistGuard implements CanActivate {
+  constructor(
+    private reflector: Reflector,
+    private jwtService: JwtService,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (isPublic) {
+      return true;
+    }
+
+    const request = context.switchToHttp().getRequest<Request>();
+    const authHeader = request.headers.authorization;
+    const token = authHeader?.replace('Bearer ', '');
+
+    if (!token) {
+      throw new UnauthorizedException('Token not found');
+    }
+
+    // 토큰 블랙리스트 확인
+    const isBlacklisted = await this.jwtService.isTokenBlacklisted(token);
+    if (isBlacklisted) {
+      throw new UnauthorizedException('Token has been revoked');
+    }
+
+    return true;
+  }
+}
