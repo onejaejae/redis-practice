65f886ab9859cf04f6e98a387b854b80d307dd35
wonjjee <yoteamo7@naver.com>
2025-07-05 14:20:49 +0900

    feat: 가드 전역 설정 & 유저 엔터티 수정

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -1,6 +1,6 @@
 import { ClassProvider, Global, Module } from '@nestjs/common';
 import { ConfigModule } from './config/config.module';
-import { APP_INTERCEPTOR } from '@nestjs/core';
+import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 import { ApiResponseInterceptor } from './interceptor/apiResponse.interceptor';
 import { ErrorInterceptor } from './interceptor/error.interceptor';
 import { TypeOrmModule } from './database/typeorm/typeorm.module';
@@ -8,19 +8,32 @@ import { LoggerModule } from './logger/logger.module';
 import { CacheModule } from './cache/cache.module';
 import { MongooseModule } from './database/mongoose/mongoose.module';
 import { LogModule } from 'src/modules/log/log.module';
+import { JwtModule } from './jwt/jwt.module';
+import { JwtBlacklistGuard } from './guard/jwtBlacklist.guard';
+import { AccessTokenGuard } from './guard/accessToken.guard';
 
-const modules = [ConfigModule, LoggerModule, CacheModule, LogModule];
+const modules = [ConfigModule, LoggerModule, CacheModule, LogModule, JwtModule];
 const providers: ClassProvider[] = [];
 const interceptors: ClassProvider[] = [
   { provide: APP_INTERCEPTOR, useClass: ErrorInterceptor },
   { provide: APP_INTERCEPTOR, useClass: ApiResponseInterceptor },
+];
+const guards: ClassProvider[] = [
+  {
+    provide: APP_GUARD,
+    useClass: JwtBlacklistGuard,
+  },
+  {
+    provide: APP_GUARD,
+    useClass: AccessTokenGuard,
+  },
 ];
 const filters: ClassProvider[] = [];
 
 @Global()
 @Module({
   imports: [TypeOrmModule.forRoot(), MongooseModule.forRoot(), ...modules],
-  providers: [...providers, ...interceptors, ...filters],
+  providers: [...providers, ...interceptors, ...filters, ...guards],
   exports: [...modules, ...providers],
 })
 export class CoreModule {}
--- src/entities/user/user.entity.ts
+++ src/entities/user/user.entity.ts
@@ -3,8 +3,14 @@ import { UuidEntity } from 'src/core/database/typeorm/base.entity';
 
 @Entity('user')
 export class User extends UuidEntity {
+  @Column({ type: 'varchar', length: 100, unique: true })
+  email: string;
+
   @Column({ type: 'varchar', length: 100 })
   name: string;
+
+  @Column({ type: 'varchar', length: 100 })
+  password: string;
 
   @Column({ type: 'int', default: 0 })
   score: number;
